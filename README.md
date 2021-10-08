424ca1a9d22a2f628dbf70aed11f0c9e004318ce
Adam Elchert <aelchert@mux.com>
2021-10-08 11:04:47 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# mux-delete-all-assets
+# MUX - Delete all assets
 Scripts in a variety of languages to delete all assets from an environment
+
+## How It Works
+
+These scripts will **DELETE ALL ASSETS** in the environment associated with the Token and Secret IDs. Please use with caution. 
+
+**Python Requirements**
+* Requires [Mux-Python](https://github.com/muxinc/mux-python)
+* Python 3.6+
+
+After cloning this repo:
+* Edit script to include the desired environments Token and Secret. For more information see the [Mux-Python](https://github.com/muxinc/mux-python) repo. 
+* Run ```python deleteAllAssets.py```
+* It will calculate how many assets you have in that environment and after confirmation will delete one asset per second.
---
+++ deleteAllAssets.py
@@ -0,0 +1,61 @@
+import mux_python
+from mux_python.rest import ApiException
+import time
+
+class color():
+    BOLD = '\033[1m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m' 
+    END = '\033[0m'
+
+# Authentication Setup
+configuration = mux_python.Configuration()
+configuration.username = 'e8a597d3-33ee-48e0-9076-266815d016be'
+configuration.password = 'WeecA/4XnBpSs9McQttyQddzF9psxgois6Z6ankjOS2TNfBiIIPs2HDajh2s8rvJSExbKPwnw3k'
+
+# API Client Initialization
+assets_api = mux_python.AssetsApi(mux_python.ApiClient(configuration))
+
+# A list of all assetIDs to be deleted
+listOfAllAssets = []
+
+
+# Collect all asset IDs
+currentPageNumber = 1
+while True:
+    list_assets_response = assets_api.list_assets(limit=25, page = currentPageNumber)
+    if len(list_assets_response.data) != 0:
+        for item in list_assets_response.data:
+            listOfAllAssets.append(item.id)
+    else:
+        break
+    currentPageNumber += 1
+
+    
+while len(listOfAllAssets) > 0: #confirm you have assets to delete
+
+    print(f"Are you sure you want to delete {color.BOLD}{color.RED} {len(listOfAllAssets)} {color.END} assets? [y/n]")
+    deleteAnAsset = input()
+
+    while deleteAnAsset.lower().strip() in ['y','n']:
+        if deleteAnAsset == 'n': 
+            exit(0)            
+        else:
+            for asset in listOfAllAssets:
+                print(f'{color.YELLOW}Deleting Asset ID:{color.END} {asset}')
+                asset = assets_api.delete_asset(asset)
+                if asset is None: #api returns "none" if successful
+                    print("Asset Deleted ✅")
+                    time.sleep(1)
+                else:
+                    print("Error") #error handling to be added
+                    exit(1)
+            else:
+                print(f"{color.GREEN}All deleted!{color.END}")
+                exit(0)
+    else: 
+        print("You must choose Y or N")
+else:
+    print("No assets in this environment to delete.")
+    exit(0)
